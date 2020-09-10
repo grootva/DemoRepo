58036706e7618f953fca723e570a9b2c1bdf9c83
grootva <41168552+arickGrootveld@users.noreply.github.com>
2020-09-10 12:49:02 -0700

    Update IndepentdentDataGen.R

--- IndepentdentDataGen.R
+++ IndepentdentDataGen.R
@@ -6,15 +6,15 @@
 
 ## Parameters of first distribution
 # Number of samples from first dist.
-N1 <- 30
-mean1 <- 0.2
-sd1 <- 0.1
+N1 <- 40
+mean1 <- 0.1
+sd1 <- 0.2
 
 ## Parameters of second distribution
 # Number of samples from second dist.
-N2 <- 10
-mean2 <- 0.5
-sd2 <- 0.1
+N2 <- 40
+mean2 <- 0.1
+sd2 <- 0.3
 
 # Generating the series of samples
 s1 <- rnorm(N1, mean=mean1, sd=sd1)
