8ad19a422269900145e6dc6a587bbc381e468df5
grootva <41168552+arickGrootveld@users.noreply.github.com>
2020-09-10 12:18:30 -0700

    Updating Sturct Breaks code
    See title

--- StructBreaksARDatagen.R
+++ StructBreaksARDatagen.R
@@ -4,10 +4,10 @@
 # Setting the rng seed to allow reproduceable runs
 set.seed(1)
 # Length of the first AR process
-n1 = 10
+n1 = 20
 
 # Length of the second AR process
-n2 = 20
+n2 = 30
 
 arOrder = c(1,0,0)
 arCoeff = 0.1
